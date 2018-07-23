0ebd73b80d2e95152674d35a22a2f14e9a887897
Chinmay Pandhare <ccpandhare@fb.com>
2018-07-24 00:07:24 +0100

    Basic Webhook

--- api-src/controllers/N00b.js
+++ api-src/controllers/N00b.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-import {orgName} from '../../.n00brc';
+import sha1 from 'sha1';
+import {orgName, APISecret} from '../../.n00brc';
 import github from '../github';
 
 const N00b = mongoose.model('N00b');
@@ -101,7 +102,7 @@ const create_new_n00b = (req, res) => {
 
 const kill_n00b = (req, res) => {
   if(req.body._id) {
-    N00b.remove({_id: req.body._id}, (err, n00b) => {
+    N00b.removeOne({_id: req.body._id}, (err, n00b) => {
       if(err || !n00b) {
         res.json({
           okay: false,
@@ -130,7 +131,7 @@ const kill_n00b = (req, res) => {
 
 const pull_n00b = (req, res) => {
   if(req.body._id) {
-    N00b.find({_id: req.body._id}, (err, n00b) => {
+    N00b.findOne({_id: req.body._id}, (err, n00b) => {
       if(err || !n00b) {
         res.json({
           okay: false,
@@ -166,9 +167,51 @@ const pull_n00b = (req, res) => {
   }
 };
 
+const webhook = (req, res) => {
+  const signature = req.headers['X-Hub-Signature'];
+  const sha = 'sha1=' + sha1(APISecret);
+  const repository = req.body.repository.name;
+  if(signature === sha) {
+    N00b.findOne({repository: repository}, (err, n00b) => {
+      if(err || !n00b) {
+        res.json({
+          okay: false,
+          error: err,
+          error_src: 'webhook.findOne'
+        });
+      } else {
+        n00b.pull().then(() => {
+          res.json({
+            okay: true,
+            last_pull: n00b.last_pull
+          });
+        }).catch(err => {
+          res.json({
+            okay: false,
+            error: err,
+            error_src: 'webhook.pull'
+          });
+        });
+      }
+    });
+    res.json({
+      okay: true
+    });
+  } else {
+    res.json({
+      okay: false,
+      error: {
+        message: 'Could not verify identity'
+      },
+      error_src: 'webhook.insecure'
+    });
+  }
+};
+
 module.exports = {
   list_all_n00bs: list_all_n00bs,
   create_new_n00b: create_new_n00b,
   kill_n00b: kill_n00b,
-  pull_n00b: pull_n00b
+  pull_n00b: pull_n00b,
+  webhook: webhook
 };
